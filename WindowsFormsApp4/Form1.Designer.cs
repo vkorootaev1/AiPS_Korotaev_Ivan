998c2ea3c47e6b9f18bde23c0c8e7894690ca5cf
Иван <@DESKTOP-43LHP1P>
2022-04-23 18:06:48 +0500

    Добавьте файлы проекта.

---
+++ WindowsFormsApp4/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿
+namespace WindowsFormsApp4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(28, 70);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(324, 40);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Выбрать файл и декодировать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(28, 116);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(324, 26);
+            this.textBox1.TabIndex = 3;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Дискретное преобразование Фурье",
+            "Быстрое преобразование Фурье"});
+            this.comboBox1.Location = new System.Drawing.Point(28, 36);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(324, 28);
+            this.comboBox1.TabIndex = 6;
+            this.comboBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(354, 24);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Выберите способ преобразования";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(61, 156);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(270, 20);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Время выполнения программы";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(126, 179);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(112, 21);
+            this.textBox2.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(375, 204);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ WindowsFormsApp4/Form1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Numerics;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        static int N = 128; // размерность
+        double[] data = new double[N]; // массив для данных, которые считываются из файла
+        double[] real = new double[N]; // массив вещественной части
+        double[] imag = new double[N]; // массив мнимой части
+        double[] result = new double[N]; // массив для результа
+        // Коротаев Иван БАС-19
+        // 6 вариант; зашифрованное слово - CARINA
+
+        private void button1_Click(object sender, EventArgs e) // Обработчик событий нажатия на кнопку
+        {
+            Stopwatch watch = new Stopwatch(); // Засекаем время выполнения программы 
+            if (comboBox1.SelectedIndex > -1)
+            {
+                // Если выбрали дискретное преобразование фурье
+                try
+                {
+                    if (comboBox1.SelectedIndex == 0)
+                    {
+
+                        var path = File_name(); // находим путь к выбранному файлу
+                        Reading(data, path, N); // считываем данные из файла в массив
+                        watch.Start();
+                        for (int k = 0; k < N; k++) // дискретное преобразование Фурье
+                        {
+                            for (int n = 0; n < N; n++)
+                            {
+                                real[k] += data[n] * Math.Cos(Coeff(k, n, N));
+                                imag[k] += data[n] * Math.Sin(Coeff(k, n, N));
+                            }
+                            result[k] = Math.Round(Math.Sqrt(real[k] * real[k] + imag[k] * imag[k]) / N * 2, 0); // Получение вещественной амплитуды после преобразования 
+                        }
+                        watch.Stop();
+                        string str = Decoding(result); // декодируем текст по ascii таблице
+                        textBox1.Text = str;
+                    }
+
+                    //Если выбрали быстрое дискретное преобразование Фурье
+
+                    else
+                    {
+                        var path = File_name();  // находим путь к выбранному файлу
+                        Reading(data, path, N); // считываем данные из файла в массив
+                        watch.Start();
+                        var data_complex = Double2Complex(N, data); // переводим наши данные в комплексный вид
+                        Complex[] result_complex = Fast(data_complex); // Вызов быстрого преобразование Фурье
+                        double[] result = Complex2Double(result_complex, N); // Возвращаем данные из комплексного вида и получение вещественной амплитуды преобразованного сигнала
+                        watch.Stop();
+                        string str = Decoding(result); // декодируем текст по ascii таблице
+                        textBox1.Text = str;
+                    }
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show("Не удалось выполнить!\n" +ex.Message, "Ошибка");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Выберите способ", "Ошибка");
+            }
+            textBox2.Text = watch.ElapsedTicks.ToString() + " тактов";
+        }
+
+        private double Coeff(int k,int n,int N) // Нахождение коэффицента для стандартного дискретного преобразования Фурье
+        {
+            double coeff = 2 * Math.PI * n * k/N;
+            return coeff;
+        }
+
+        private string File_name() // Выбор файла для считывания 
+        {
+            var dialog = new OpenFileDialog();
+            dialog.Filter = "TXT|* .txt";
+            if (dialog.ShowDialog() != DialogResult.OK)
+            {
+                MessageBox.Show("Вы не выбрали файл!");
+            }
+            var str = dialog.FileName;
+            return str;
+        }
+        private  double [] Reading( double[] data,string path,int N) // Считывание данных из файла
+        {
+            StreamReader sr = new StreamReader(path);
+            for (int i = 0; i < N; i++)
+            {
+                data[i] = Double.Parse(sr.ReadLine());
+            }
+            sr.Close();
+            return data;
+        }
+
+        private string Decoding(double [] result) // Декодирование результативных данных по таблице ascii
+        {
+            string str ="";
+            for (int i = 1; i < 7; i++)
+            {
+                if (result[i] != 0)
+                {
+                    str += (char)result[i];
+                }
+            }
+            return str;
+        }
+
+        private Complex Coeff(int k, int N) // Получение коэффицента
+        {
+            Complex coeff;
+            double argument = -2 * Math.PI * k / N;
+            coeff = new Complex(Math.Cos(argument), Math.Sin(argument));
+            return coeff;
+        }
+
+        private double[] Complex2Double(Complex [] result_complex,int N) // Функция перевода из комлексного вида в вещественный
+        {
+            double[] result = new double[N];
+            for(int i =0; i<N;i++ )
+            {
+                result[i] = Math.Round(Math.Sqrt(result_complex[i].Real* result_complex[i].Real + result_complex[i].Imaginary * result_complex[i].Imaginary) / N * 2.0,0); // Получение амплитуды после преобразования 
+            }
+            return result;
+        }
+
+        private Complex[] Double2Complex(int N,double [] data) // Перевод из вещественного вида в комплексный
+        {
+            Complex [] complex = new Complex[N];
+            for(int i = 0; i<N;i++)
+            {
+                complex[i] = new Complex (data[i], 0);
+            }
+            return complex;
+        }
+
+
+        private Complex[] Fast(Complex [] data_complex) // Рекурсивная функция быстрого преобразование Фурье (каждый раз вдвое уменьшаем размерность)
+        {
+            int N = data_complex.Length; // Текущая размерность
+            Complex[] result;
+            if (N == 2) // Если размерность равна 2 (т.е. больше уменьшить размерность нельзя)
+            {
+                result = new Complex[2];
+                result[0] = data_complex[0] + data_complex[1]; // значение с четным номером
+                result[1] = data_complex[0] - data_complex[1]; // значение с нечетным номером
+            }
+            else // Разбиение сигнала на 2
+            {
+                Complex[] data_complex_chet = new Complex[N / 2]; // Создаем массив с размерностью в два раза меньше предыдущей (четные номера)
+                Complex[] data_complex_nechet = new Complex[N / 2]; // Создаем массив с размерностью в два раза меньше предыдущей (нечетные номера)
+                for (int i = 0; i < N / 2; i++)
+                {
+                    data_complex_chet[i] = data_complex[2 * i]; 
+                    data_complex_nechet[i] = data_complex[2 * i + 1];
+                }
+                Complex[] result_chet = Fast(data_complex_chet); // повторение БПФ (четные номера)
+                Complex[] result_nechet = Fast(data_complex_nechet); // повторение БПФ (нечетные номера)
+                result = new Complex[N];
+                for (int i = 0; i < N / 2; i++) //"Сборка" преобразованного сигнала
+                {
+                    result[i] = result_chet[i] + Coeff(i, N) * result_nechet[i];
+                    result[i + N / 2] = result_chet[i] - Coeff(i, N) * result_nechet[i];
+                }
+            }
+            return result;
+        }
+    }
+}
